1fae458d47523f34230350dcecf6386102e1a2a8
octador <mutuon.j@gmail.com>
2024-02-26 15:38:43 +0100

    jo symfo

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+
+
+   }
+        #[Route('/creat', name: 'app_creat')]
+        public function creat(): Response
+    {
+        return $this->render('home/creat.html.twig', [
+            
+        ]);
+    }
+
+ 
+}
